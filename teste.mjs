dd1fcd02c90f26cd594ed8d60dc1277aa5d1ce50
uBrunoow <brunowerner7@gmail.com>
2024-03-15 15:50:05 -0300

    update

---
+++ teste.mjs
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+"use strict";
+
+import chalk from 'chalk';
+
+console.log(chalk.blue.bold('Bem-vindo ao executável boilerplate da Aupi. Para rodar isso você precisa estar em um Linux!'));
