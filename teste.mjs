e67fc25961f38d8c996b30d5d902c301c810fee3
uBrunoow <brunowerner7@gmail.com>
2024-03-16 19:49:49 -0300

    teste do workflow

--- teste.mjs
+++ teste.mjs
@@ -3,4 +3,4 @@
 
 import chalk from 'chalk';
 
-console.log(chalk.blue.bold('Bem-vindo ao executável boilerplate da Aupi. Para rodar isso você precisa estar em um Linux!'));
+console.log(chalk.blue.bold('AAAAAAABem-vindo ao executável boilerplate da Aupi. Para rodar isso você precisa estar em um Linux!'));
